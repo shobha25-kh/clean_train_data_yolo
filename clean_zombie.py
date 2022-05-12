f50f6074a0b8b5e8bc8ac383a0aa85ac435a8d95
shobha25-kh <105263959+shobha25-kh@users.noreply.github.com>
2022-05-12 12:17:34 +0530

    Update clean_zombie.py

--- clean_zombie.py
+++ clean_zombie.py
@@ -1,19 +1,23 @@
 import os
 from pathlib import Path
 path = "./obj/" #folder path
-dir_list = os.listdir(path)
 
-for x in dir_list:
-    if x.endswith(".jpg"):
-        #print(x)
-        y=os.path.splitext(x)[0]
-        z = ".txt"
-        s = y + z
-        k = Path("./obj/" +s)
-        #print(k)
-        if k.is_file():
+def clean_image(path):
+    dir_list = os.listdir(path)
+    for x in dir_list:
+        if x.endswith(".jpg"):
+            #print(x)
+            y=os.path.splitext(x)[0]
+            z = ".txt"
+            s = y + z
+            k = Path(path+s)
+            #print(k)
+            if k.is_file()
             print("txt file present\n", s)
-        else:
-            print("!!!!!!!!!!!!!!!!removing jpg file!!!!!!!!!!!!\n")
-            os.remove("./obj/"+x)
-        # Prints only text file present in My Folder
+            else:
+                print("!!!!!!!!!!!!!!!!removing jpg file!!!!!!!!!!!!\n")
+                os.remove(path+x)
+ 
+clean_image(path)
+#def clean_empty_txt(path):
+    

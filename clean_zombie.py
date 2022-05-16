abe349fbd066037848a686fe1c45454728983921
shobha25-kh <105263959+shobha25-kh@users.noreply.github.com>
2022-05-16 22:10:15 +0530

    Update clean_zombie.py

--- clean_zombie.py
+++ clean_zombie.py
@@ -2,8 +2,23 @@ import sys
 import os
 from pathlib import Path
 
-user_input = input("Enter the path of your file: ")
-assert os.path.exists(user_input),  "\n" + str(user_input) +", This path does not exist"
+def main(args):
+    if(len(args)<2):
+        sys.stderr.write("Usage: {} [image folder path] [0:CVAT data|1: unlabel image]\n".format(args[0]))
+        sys.exit(1)
+    if len(args)== 3:
+        user_input = args[2]
+        assert os.path.exists(user_input),  "\n" + str(user_input) +", This path does not exist" 
+        if args[3] == '0':
+            clean_empty_txt(user_input+"/")
+        elif args[3] == '1':
+            clean_image(user_input+"/")
+        else:
+            print(" Invalid argument for program \n")
+            sys.exit(1)
+        
+        
+    
 
 def clean_empty_txt(path):
     dir_list = os.listdir(path)
@@ -34,8 +49,9 @@ def clean_image(path):
                 print("!!!!!!!!!!!!!!!!removing jpg file!!!!!!!!!!!!\n")
                 os.remove(path+x)
  
-#clean_image(user_input+"/")
-clean_empty_txt(user_input+"/")
 
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
 
     

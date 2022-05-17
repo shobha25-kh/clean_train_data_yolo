d21aa7eb62a3c61bd9dd84cd501ec20b125cc307
shobha25-kh <105263959+shobha25-kh@users.noreply.github.com>
2022-05-17 10:23:57 +0530

    Update clean_zombie.py

--- clean_zombie.py
+++ clean_zombie.py
@@ -2,16 +2,18 @@ import sys
 import os
 from pathlib import Path
 
+count = 0
 def main(args):
     if(len(args)<2):
         sys.stderr.write("Usage: {} [image folder path] [0:CVAT data|1: unlabel image]\n".format(args[0]))
         sys.exit(1)
     if len(args)== 3:
-        user_input = args[2]
+        user_input = args[1]
         assert os.path.exists(user_input),  "\n" + str(user_input) +", This path does not exist" 
-        if args[3] == '0':
+        if args[2] == '0':
             clean_empty_txt(user_input+"/")
-        elif args[3] == '1':
+            print(f'Total files, {count} deleted')
+        elif args[2] == '1':
             clean_image(user_input+"/")
         else:
             print(" Invalid argument for program \n")
@@ -22,21 +24,25 @@ def main(args):
 
 def clean_empty_txt(path):
     dir_list = os.listdir(path)
+    global count
     for txt in dir_list:
         if txt.endswith(".txt"):
             if not os.path.getsize(path+txt):
-                print(txt)
+                print(f'Empty {txt} file')
                 y=os.path.splitext(txt)[0]
                 s = Path(path+y+".jpg")
                 if s.is_file():
-                    print("### removing respective image ###\n",s)
+                    print(" Removing respective image: ",s)
                     os.remove(s)
                 os.remove(path+txt)
+                count += 1
             else:
                 continue
 
+
 def clean_image(path):
     dir_list = os.listdir(path)
+    global count
     for x in dir_list:
         if x.endswith(".jpg"):
             #print(x)
@@ -48,10 +54,10 @@ def clean_image(path):
             else:
                 print("!!!!!!!!!!!!!!!!removing jpg file!!!!!!!!!!!!\n")
                 os.remove(path+x)
+                count +=1
+                
  
 
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
-
-    
